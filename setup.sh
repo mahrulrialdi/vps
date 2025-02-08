508d9c4549ed27f26f4abc7e3381ebbc098105f0
mahrulrialdi <mahrulrialdi@gmail.com>
2025-02-08 12:45:49 +0700

    update

--- setup.sh
+++ setup.sh
@@ -52,35 +52,35 @@ exit 0
 fi
 mkdir /var/lib/mrnetworkvpn;
 echo "IP=" >> /var/lib/mrnetworkvpn/ipvps.conf
-# wget https://${mrnetworkvpn}/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://${mrnetworkvpn}/cf.sh && chmod +x cf.sh && ./cf.sh
 #install v2ray
 wget https://${mrnetworkxray}/ins-xray.sh && chmod +x ins-xray.sh && screen -S xray ./ins-xray.sh
 #install ssh ovpn
-# wget https://${mrnetworkvpn}/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-# wget https://${mrnetworksstp}/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://${mrnetworkvpn}/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://${mrnetworksstp}/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
 #install ssr
-# wget https://${mrnetworkssr}/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-# wget https://${mrnetworkss}/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+wget https://${mrnetworkssr}/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://${mrnetworkss}/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
 #installwg
-# wget https://${mrnetworkwr}/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://${mrnetworkwr}/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install L2TP
-# wget https://${mrnetworkipsec}/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-# wget https://${mrnetworkbackup}/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+wget https://${mrnetworkipsec}/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://${mrnetworkbackup}/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 # Websocket
-# wget https://${mrnetworkws}/edu.sh && chmod +x edu.sh && ./edu.sh
+wget https://${mrnetworkws}/edu.sh && chmod +x edu.sh && ./edu.sh
 # Ohp Server
-# wget https://${mrnetworkohp}/ohp.sh && chmod +x ohp.sh && ./ohp.sh
+wget https://${mrnetworkohp}/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 
-# rm -f /root/ssh-vpn.sh
-# rm -f /root/sstp.sh
-# rm -f /root/wg.sh
-# rm -f /root/ss.sh
-# rm -f /root/ssr.sh
+rm -f /root/ssh-vpn.sh
+rm -f /root/sstp.sh
+rm -f /root/wg.sh
+rm -f /root/ss.sh
+rm -f /root/ssr.sh
 rm -f /root/ins-xray.sh
-# rm -f /root/ipsec.sh
-# rm -f /root/set-br.sh
-# rm -f /root/edu.sh
-# rm -f /root/ohp.sh
+rm -f /root/ipsec.sh
+rm -f /root/set-br.sh
+rm -f /root/edu.sh
+rm -f /root/ohp.sh
 cat <<EOF> /etc/systemd/system/autosett.service
 [Unit]
 Description=autosetting
